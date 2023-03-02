7db7fa4c972c33f41104e16fb5ac5bd2d8fb855b
Gun <thanapat2027@gmail.com>
2023-03-02 18:40:59 +0700

    uploaded init files

---
+++ level_manager.gd
@@ -0,0 +1,11 @@
+extends Node
+# Level Manager
+
+@onready var map = Global.Map
+
+func _ready():
+	var size = Vector2i(10,10)
+	for i in size.x:
+		for j in size.y:
+			map.set_cell(0,Vector2(i,j),0,Vector2i(32+randi()%3,1))
+	
