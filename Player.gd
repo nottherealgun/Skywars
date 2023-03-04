2c2cb2dd878633fadfe6ebd78cd11b66a2d6b3d7
NotTheRealGun <thanapat2027@gmail.com>
2023-03-04 18:36:51 +0700

    Added basic HUD & player device awareness

--- Player.gd
+++ Player.gd
@@ -1,15 +1,30 @@
 extends Node2D
 
 @export var player_id = 1
+@export_enum("darwin","gun") var character = "darwin"
+var player_color = [Color.DODGER_BLUE,Color.CORAL,Color.DARK_GREEN,Color.MEDIUM_PURPLE][player_id-1]
 @export var max_health = 10
 var health = max_health
+@export var stress = 0
+var max_stress = 100
 @export var speed = 1
+
 var move_vec := Vector2.ZERO
 var aim_vec := Vector2.ZERO
 var fainted = false
 
+@onready var stat_gui = Global.GUI.get_node("PlayerStats"+str(player_id))
+@onready var healthbar = stat_gui.get_node("Healthbar")
+@onready var stressbar = stat_gui.get_node("Stressbar")
+func _ready():
+	$Arrow.modulate = player_color
+	stat_gui.show()
+	stat_gui.get_node("NameTag").set("theme_override_colors/font_color",player_color)
+	stat_gui.get_node("NameTag").text = character
+	
 func _process(delta):
 	_input_update()
+	_gui_update()
 	var sprite = get_node("Sprite")
 	if health <= 0 and !fainted:
 		sprite.play("faint")
@@ -37,7 +52,11 @@ func _input_update():
 					shoot()
 		if Input.is_action_just_pressed("p"+str(player_id)+"_action"):
 			pass
-				
+
+func _gui_update():
+	healthbar.value = (health*healthbar.max_value)/max_health
+	stressbar.value = (stress*stressbar.max_value)/max_stress
+
 func _move_update(delta):
 	move_vec = Vector2.ZERO
 	if Input.is_action_pressed("p"+str(player_id)+"_up"):
--- game_manager.gd
+++ game_manager.gd
@@ -2,8 +2,18 @@ extends Node
 
 @onready var cam = get_node("/root/Main/MainCam")
 var cam_follow_target = Vector2.ZERO
+var active_players = []
+
+func _ready():
+	for n in Global.Main.get_children():
+		if n.is_in_group("player"):
+			active_players.append(n)
 
 func _process(delta):
-	cam_follow_target = Global.Main.get_node("Player").position
+	var vec := Vector2.ZERO
+	for p in active_players:
+		vec += p.position
+	vec/=active_players.size()
+	cam_follow_target = vec
 	cam.position = cam_follow_target
 	Global.Dev.text = str(Global.Main.get_node("Player").health)
