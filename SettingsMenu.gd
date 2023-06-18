f8c918190183ee9cae649bb7c4978e7add39e19d
NotTheRealGun <thanapat2027@gmail.com>
2023-06-18 22:17:20 +0700

    Fixed pause screen settings button not showing

--- SettingsMenu.gd
+++ SettingsMenu.gd
@@ -2,3 +2,8 @@ extends Control
 
 func _on_back_pressed():
 	$AnimationPlayer.play("exit")
+
+func show():
+	super()
+	var t = create_tween().set_trans(Tween.TRANS_CUBIC)
+	t.tween_property(self,"position",Vector2.ZERO,1.0)
